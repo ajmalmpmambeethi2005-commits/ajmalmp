0917c8c2bf92ff232a06c4871f9df4fa53c66338
ajmalmp <ajmalmpmambeethi2005@gmail.com>
2026-09-11 11:07:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is Home Page"
+    student = "Ajmal"
+    age = 20
 
-@app.route("/about")
-def about():
-    return "This is About Page"
-
-@app.route("/contact")
-def contact():
-    return "This is Contact Page"
-
+    return render_template(
+        "home.html",
+        student=student,
+        age=age
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
