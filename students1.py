c3054b1eb5874094e7541f5ba2a23e67e8b5f1bb
ajmalmp <ajmalmpmambeethi2005@gmail.com>
2026-09-14 10:38:46 +0530

    Add files via upload

--- students1.py
+++ students1.py
@@ -1,6 +1,30 @@
+# import pandas as pd
+
+# mf=pd.read_csv("student.csv")
+
+# print(mf)
+
+# import pandas as pd
+
+# mf = pd.read_csv("students.csv")
+
+# print(mf)
+
+# for index, row in df.iterrows():
+#     print(row["Name"])
+
 import pandas as pd
 
-df = pd.read_csv("students.csv")
+df=pd.read_csv("students.csv")
 
 for index, row in df.iterrows():
-    print(row["Name"], row["Marks"])
+    print(row["Name"],row["Marks"])
+
+# import pandas as pd
+
+# df = pd.read_csv("students.csv")
+
+# df.columns = df.columns.str.strip()
+
+# for index, row in df.iterrows():
+#     print(row["Name"], row["Marks"])
