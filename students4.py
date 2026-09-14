cdf1b2d75ed173f98c979e555d5ecce98e2b74d1
ajmalmp <ajmalmpmambeethi2005@gmail.com>
2026-09-14 17:18:59 +0530

    Add files via upload

---
+++ students4.py
@@ -0,0 +1,17 @@
+# def calculate_total(numbers):
+#     return sum(numbers)
+
+
+# numbers = [10, 20, 30]
+# print(calculate_total(numbers))
+
+# numbers2 = [5, 15, 25]
+# print(calculate_total(numbers2))
+
+def ajmal(numbers):
+    return sum(numbers)
+numbers=[10,18,161,14]
+print(ajmal(numbers))
+
+numbers=[12,2,3,4,5]
+print(ajmal(numbers))
