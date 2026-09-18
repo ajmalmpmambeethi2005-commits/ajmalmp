f5f3cb536976438fbe58e7e91900c0581a8f629e
ajmalmp <ajmalmpmambeethi2005@gmail.com>
2026-09-18 12:00:26 +0530

    Add files via upload

---
+++ app6.py
@@ -0,0 +1,24 @@
+# import pandas as pd
+
+# data={
+#     "name":["ajmal","ashmal","sinan"],
+#     "age":[21,22,23],
+#     "mark":[90,32,34]
+
+# }
+# df=pd.DataFrame(data)
+# print(df)
+
+
+
+
+import pandas as pd
+
+data={
+    "name":["ajmal","amal","lalu"],
+    "age":[12,21,23],
+    "mark":[12,23,34]
+}
+
+df=pd.DataFrame(data)
+print(df)
