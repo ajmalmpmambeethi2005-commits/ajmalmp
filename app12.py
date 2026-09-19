0bca725ef9bf85ae9fae43453f83acaf4c9c79ec
ajmalmp <ajmalmpmambeethi2005@gmail.com>
2026-09-19 12:16:36 +0530

    Add files via upload

---
+++ app12.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify
+
+app=Flask(__name__)
+
+@app.route("/student")
+def student():
+
+    return jsonify({
+        "name":"ajmal",
+        "age":21,
+        "course":"stati"
+
+    })
+if __name__=="__main__":
+    app.run(debug="True")
